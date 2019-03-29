87a1a23eb2a552c0273e927beefa8dc285db845a
Hadron67 <604700341@qq.com>
2019-03-29 12:38:50 +0800

    added markdown and sass generator

--- index.js
+++ index.js
@@ -4,23 +4,33 @@ let cp = require('./lib/component.js');
 
 let app = require('./lib/main.js')();
 
+let metaTags = cp.template(
+    '<meta charset="utf-8" />',
+    '<meta http-equiv="X-UA-Compatible" content="IE=edge" />',
+    '<meta viewport="width=device-width, initial-scale=1" />'
+);
+
+class TestTemplate {
+    constructor(content){
+        
+    }
+};
+
+let html = cp.template(
+    '<html>',
+        '<head>',
+            '<title>',
+                a => a.config.title,
+            '</title>',
+            metaTags,
+            '<link href="css/main.css" rel="stylesheet">',
+        '</head>',
+        '<body>',
+            
+        '</body>',
+    '</html>'
+);
 function mainPage(){
-    let metaTags = cp.template(
-        '<meta charset="utf-8" />',
-        '<meta http-equiv="X-UA-Compatible" content="IE=edge" />',
-        '<meta viewport="width=device-width, initial-scale=1" />'
-    );
-    
-    let html = cp.template(
-        '<html>',
-            '<head>',
-                '<title>',
-                    a => a.config.title,
-                '</title>',
-                metaTags,
-            '</head>',
-        '</html>'
-    );
 
     let config = {title: 'Test'};
 
@@ -28,10 +38,14 @@ function mainPage(){
         handle(resolve, reject){
             resolve(cp.render(html, {config}));
         },
-        isStatic: false
+        isStatic: false,
+        mime: 'text/html'
     };
 }
 
 app.registerRaw('/index.html', mainPage());
+
+app.markdown.register('src/posts/test.md', '/article/test.html');
+app.scss.register('/css/main.css', 'src/sass/main.scss');
 
 app.startServer(8080);
---
+++ src/posts/test.md
@@ -0,0 +1,9 @@
+---
+layout: hkmg
+---
+# Test
+This is a test page $\vec B = \nabla\times\vec A$
+rtherth
+
+sdfsdf
+> rthey
